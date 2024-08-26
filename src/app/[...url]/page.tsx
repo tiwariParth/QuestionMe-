8a8fb166facd28438d5fd6b9d2c1639c8ad4edf1
tiwariParth <243mjparth@gmail.com>
2024-08-26 18:03:25 +0530

    chore: Update npm dependencies and add rag-chat library

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,3 +1,5 @@
+import { ragChat } from "@/lib/rag-chat";
+import { url } from "inspector";
 import React from "react";
 
 interface PageProps {
@@ -6,7 +8,20 @@ interface PageProps {
   };
 }
 
-const page = ({ params }: PageProps) => {
+const reconstructURL = ({ url }: { url: string[] }) => {
+  const deconstructUrl = url.map((component) => {
+    decodeURIComponent(component);
+  });
+  return deconstructUrl.join("/");
+};
+
+const page = async ({ params }: PageProps) => {
+  const reconstructUrl = reconstructURL({ url: params.url as string[] });
+  await ragChat.context.add({
+    type: "html",
+    source: reconstructUrl,
+    config: { chunkOverlap: 50, chunkSize: 200 },
+  });
   return <div>page</div>;
 };
 
