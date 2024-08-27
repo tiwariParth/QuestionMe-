32e52ff06ebc65f29865f2b76140906ad4698377
tiwariParth <243mjparth@gmail.com>
2024-08-27 16:08:19 +0530

    chore:error detection

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,5 +1,4 @@
 import { ragChat } from "@/lib/rag-chat";
-import { url } from "inspector";
 import React from "react";
 
 interface PageProps {
@@ -8,20 +7,28 @@ interface PageProps {
   };
 }
 
-const reconstructURL = ({ url }: { url: string[] }) => {
-  const deconstructUrl = url.map((component) => {
-    decodeURIComponent(component);
-  });
-  return deconstructUrl.join("/");
-};
+function reconstructURL({ url }: { url: string[] }) {
+  const decodeComponents = url.map((component) =>
+    decodeURIComponent(component)
+  );
+  console.log(decodeComponents);
+  return decodeComponents.join("/");
+}
 
 const page = async ({ params }: PageProps) => {
-  const reconstructUrl = reconstructURL({ url: params.url as string[] });
+  if (!params.url || typeof params.url === "string") {
+    throw new Error("Invalid URL parameter");
+  }
+  console.log(params);
+
+  const reconstructedUrl = reconstructURL({ url: params.url as string[] });
+
   await ragChat.context.add({
     type: "html",
-    source: reconstructUrl,
+    source: reconstructedUrl,
     config: { chunkOverlap: 50, chunkSize: 200 },
   });
+
   return <div>page</div>;
 };
 
