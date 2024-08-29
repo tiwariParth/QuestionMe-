5d96605566c796607e84ceb731b90c4b441d5657
tiwariParth <243mjparth@gmail.com>
2024-08-29 11:25:37 +0530

    feat: Fix typo in Messages component
    The commit fixes a typo in the Messages component by replacing "You're all set !" with "You're all set!" to ensure consistent and correct messaging in the user interface.

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -23,7 +23,7 @@ const Messages = ({ messages }: MessagesProp) => {
           <div className="flex-1 flex flex-col items-center justify-center gap-2">
             <MessageSquare className="size-8 text-blue-500" />
             <h3 className="font-semibold text-xl text-white">
-              You're all set !
+              You&apos;re all set!
             </h3>
             <p className="text-zinc-500 text-sm">
               Get started by asking a question
